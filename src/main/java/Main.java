f897c5d4800c8659d1a09809fc892001d5920aec
JellyPuddy <43268724+JellyPuddy@users.noreply.github.com>
2023-03-08 17:05:22 +0100

    Implement q2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,16 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.expressions.UserDefinedFunction;
+import org.apache.spark.sql.types.DataTypes;
+import scala.collection.JavaConverters;
+import scala.collection.Seq;
+
 import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
+import static org.apache.spark.sql.functions.udf;
 
 
 public class Main {
@@ -17,7 +26,7 @@ public class Main {
 
         // TODO: You may want to change ERROR to WARN to receive more info. For larger data sets, to not set the
         // log level to anything below WARN, Spark will print too much information.
-        if (onServer) javaSparkContext.setLogLevel("ERROR");
+        javaSparkContext.setLogLevel("ERROR");
 
         return javaSparkContext;
     }
@@ -27,11 +36,7 @@ public class Main {
 
         SparkSession sparkSession = SparkSession.builder().sparkContext(sparkContext.sc()).getOrCreate();
 
-        Dataset<Row> df = sparkSession.read().csv(vectorsFilePath);
-
-        df.show();
-
-        return df;
+        return sparkSession.read().csv(vectorsFilePath);
     }
 
     private static JavaRDD q1b(JavaSparkContext sparkContext, boolean onServer) {
@@ -39,79 +44,84 @@ public class Main {
 
         SparkSession sparkSession = SparkSession.builder().sparkContext(sparkContext.sc()).getOrCreate();
 
-        JavaRDD<String> RDD = sparkSession.sparkContext()
+        return sparkSession.sparkContext()
         .textFile(vectorsFilePath, 1)
         .toJavaRDD();
-        
-        JavaRDD<String> words = RDD.flatMap(line -> Arrays.asList(line.split(";")).iterator());
-        long count = words.count();
-        System.out.println("Word count: " + count);
-
-        return RDD;
-
     }
 
     private static void q2(JavaSparkContext sparkContext, Dataset dataset) {
-        //Dataset<Row> first_column = dataset.select("_c0");
-        //Dataset<Row> second_column = dataset.select("_c1");
-
         dataset.createOrReplaceTempView("dataset");
 
-        Dataset<Row> sql = dataset.sqlContext().sql("SELECT _c0 from dataset");
-        sql.show();
+        Dataset<Row> splitted = dataset.sqlContext().sql("SELECT _c0 as id, SPLIT(_c1, ';') AS values FROM dataset");
+        splitted.createOrReplaceTempView("splitted_data");
 
-        // Split the values field into an array of integers
-        dataset.sqlContext().sql("SELECT _c0 as id, SPLIT(_c1, ';') AS values FROM dataset")
-            .createOrReplaceTempView("splitted_data");
-                
-        dataset.sqlContext().sql("SELECT X.id,Y.id,Z.id AS _values FROM splitted_data as X " + 
-        "JOIN splitted_data AS Y ON X.id < Y.id JOIN splitted_data AS Z ON Y.id < Z.id")
-        .show();
-        // Compute the variance of each vector
-    //     dataset.sqlContext().sql("SELECT " +
-    //     "triplets.X_numbers, " +
-    //     "triplets.Y_numbers, " +
-    //     "triplets.Z_numbers, " +
-    //     "variance(cast(pow(cast(triplets.X_numbers[0] as double), 2) as double) + " +
-    //     "         cast(pow(cast(triplets.Y_numbers[0] as double), 2) as double) + " +
-    //     "         cast(pow(cast(triplets.Z_numbers[0] as double), 2) as double)) as varianceXYZ " +
-    //   "FROM ( " +
-    //     "SELECT " +
-    //       "t1.ID as X, " +
-    //       "t2.ID as Y, " +
-    //       "t3.ID as Z, " +
-    //       "t1.numbers as X_numbers, " +
-    //       "t2.numbers as Y_numbers, " +
-    //       "t3.numbers as Z_numbers " +
-    //     "FROM " +
-    //       "( " +
-    //         "SELECT " +
-    //           "_c0 as ID, " +
-    //           "split(_c1, ';') as numbers " +
-    //         "FROM " +
-    //           "dataset " +
-    //       ") t1 " +
-    //     "JOIN " +
-    //       "( " +
-    //         "SELECT " +
-    //           "_c0 as ID, " +
-    //           "split(_c1, ';') as numbers " +
-    //         "FROM " +
-    //           "dataset " +
-    //       ") t2 ON t1.ID < t2.ID " +
-    //     "JOIN " +
-    //       "( " +
-    //         "SELECT " +
-    //           "_c0 as ID, " +
-    //           "split(_c1, ';') as numbers " +
-    //         "FROM " +
-    //           "dataset " +
-    //       ") t3 ON t2.ID < t3.ID " +
-    //   ") AS triplets " +
-    //   "GROUP BY triplets.X_numbers, triplets.Y, triplets.Z " +
-    //   "HAVING varianceXYZ <= 410 ")
-    //   .show();
+        System.out.println("Number of vectors: " + splitted.count());
+        splitted.show(10);
 
+        UserDefinedFunction sum_var = udf(
+                (Seq<String> a, Seq<String> b, Seq<String> c) -> {
+                    // Convert Seq to List
+                    List<String> aa = JavaConverters.asJava(a);
+                    List<String> bb = JavaConverters.asJava(b);
+                    List<String> cc = JavaConverters.asJava(c);
+
+                    // Aggregate the vectors
+                    int[] result = new int[a.length()];
+                    for (int i = 0; i < a.length(); i++) {
+                        result[i] = Integer.parseInt(aa.get(i)) + Integer.parseInt(bb.get(i)) + Integer.parseInt(cc.get(i));
+                    }
+
+                    // Compute the variance
+                    OptionalDouble optAvg = Arrays.stream(result).average();
+                    if (optAvg.isEmpty()) {
+                        return 0;
+                    }
+                    double avg = optAvg.getAsDouble();
+                    double var = 0;
+                    for (int j : result) {
+                        var += Math.pow(j, 2);
+                    }
+                    var = var / result.length;
+                    return var - Math.pow(avg, 2);
+                }, DataTypes.DoubleType
+        );
+
+        // Register the function as a UDF
+        dataset.sqlContext().udf().register("sum_var", sum_var);
+
+        // Join the dataset with itself twice to get all possible triplets
+        Dataset<Row> triplets = dataset.sqlContext().sql(
+                "SELECT X.id as X_id, Y.id as Y_id, Z.id as Z_id, " +
+                "sum_var(X.values, Y.values, Z.values) AS var " +
+                "FROM " +
+                "(splitted_data as X JOIN splitted_data AS Y ON X.id < Y.id JOIN splitted_data AS Z ON Y.id < Z.id)" +
+                "ORDER BY var ASC");
+        triplets.createOrReplaceTempView("triplets");
+        // Persist the triplets so that we can reuse them for different values of tau
+        triplets.persist();
+
+        // Print the size of the triplets
+        System.out.println("Number of triplets: " + triplets.count());
+        triplets.show(10);
+
+        int[] taus = { 20, 50, 310, 360, 410 };
+        for (int tau : taus) {
+            System.out.println("Tau: " + tau);
+            // Compute the variance of each vector
+            Dataset<Row> variance = dataset.sqlContext().sql("SELECT " +
+                    "X_id, Y_id, Z_id, var " +
+                    "FROM " +
+                    "triplets " +
+                    "WHERE var <= " + tau);
+            variance.createOrReplaceTempView("variance");
+
+            // Print the size of the variance
+            System.out.println("Number of triplets: " + variance.count());
+            variance.show(10);
+        }
+
+        // Unpersist the triplets
+        triplets.unpersist();
     }
 
     private static void q3(JavaSparkContext sparkContext, JavaRDD rdd) {
@@ -126,7 +136,6 @@ public class Main {
     // Main method which initializes a Spark context and runs the code for each question.
     // To skip executing a question while developing a solution, simply comment out the corresponding method call.
     public static void main(String[] args) {
-
         boolean onServer = false; // TODO: Set this to true if and only if building a JAR to run on the server
 
         JavaSparkContext sparkContext = getSparkContext(onServer);
@@ -142,7 +151,5 @@ public class Main {
         q4(sparkContext, rdd);
 
         sparkContext.close();
-
-
     }
 }
